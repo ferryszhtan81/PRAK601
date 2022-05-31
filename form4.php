8b5794c9eba50b06ec27f82748369457464b1c17
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-05-31 11:15:38 +0800

    Add files via upload

---
+++ form4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>form4.php</title>
+</head>
+<body>
+<form method="post" action="form4submit.php">
+    Username:<br /><br /><input type="text" name="uname"><br /><br />
+    Password:<br /><br /><input type="password" name="password"><br>
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
---
+++ form4submit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>form4.php</title>
+</head>
+<body>
+<?php if (isset($_GET['error'])) { ?>
+
+    <p class="error"><?php echo $_GET['error']; ?></p>
+
+<?php } ?>
+</body>
+</html>
